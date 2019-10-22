f826642141f07e320ff96692a967321f0a4bdbdd
Juliana Adema <juliana.adema@mail.utoronto.ca>
2019-10-22 13:12:53 -0400

    Update PS_skeleton.py

--- PS_skeleton.py
+++ PS_skeleton.py
@@ -5,15 +5,15 @@ pandorable problem set 3 for PSY 1210 - Fall 2019
 
 @author: katherineduncan
 
-In this problem set, you'll practice your new pandas data management skills, 
+In this problem set, you'll practice your new pandas data management skills,
 continuing to work with the 2018 IAT data used in class
 
 Note that this is a group assignment. Please work in groups of ~4. You can divvy
-up the questions between you, or better yet, work together on the questions to 
-overcome potential hurdles 
+up the questions between you, or better yet, work together on the questions to
+overcome potential hurdles
 """
 
-#%% import packages 
+#%% import packages
 import os
 import numpy as np
 import pandas as pd
@@ -22,7 +22,8 @@ import pandas as pd
 # Question 1: reading and cleaning
 
 # read in the included IAT_2018.csv file
-IAT = ...
+data_file = 'IAT_2018.csv'
+IAT = pd.read_csv(data_file)
 
 # rename and reorder the variables to the following (original name->new name):
 # session_id->id
@@ -31,7 +32,7 @@ IAT = ...
 # edu->edu
 # politicalid_7->politic
 # STATE -> state
-# att_7->attitude 
+# att_7->attitude
 # tblacks_0to10-> tblack
 # twhites_0to10-> twhite
 # labels->labels
@@ -81,24 +82,24 @@ states =...
 # now use the pivot_table function to calculate the same statistics
 state_bias=...
 
-# make another pivot_table that calculates median bias per state, separately 
+# make another pivot_table that calculates median bias per state, separately
 # for each race (organized by columns)
 state_race_bias=...
 
 #%%
 # Question 4: merging and more merging
 
-# add a new variable that codes for whether or not a participant identifies as 
+# add a new variable that codes for whether or not a participant identifies as
 # black/African American
 
-# use your new variable along with the crosstab function to calculate the 
-# proportion of each state's population that is black 
+# use your new variable along with the crosstab function to calculate the
+# proportion of each state's population that is black
 # *hint check out the normalization options
 prop_black =...
 
 # state_pop.xlsx contains census data from 2000 taken from http://www.censusscope.org/us/rank_race_blackafricanamerican.html
-# the last column contains the proportion of residents who identify as 
-# black/African American 
+# the last column contains the proportion of residents who identify as
+# black/African American
 # read in this file and merge its contents with your prop_black table
 census =...
 merged =...
@@ -107,11 +108,6 @@ merged =...
 
 # now merge the census data with your state_race_bias pivot table
 
-# use the corr method again to determine whether white_good biases is correlated 
+# use the corr method again to determine whether white_good biases is correlated
 # with the proportion of the population which is black across states
 # calculate and print this correlation for white and black participants
-
-
-
-
-
