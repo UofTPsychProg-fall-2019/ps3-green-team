1afbff9861f685d73086f69b0988f3216ab92793
Gaeun Son <gaeun.son@mail.utoronto.ca>
2019-10-27 11:41:46 -0400

    Question 4 part 2 done

--- PS_skeleton.py
+++ PS_skeleton.py
@@ -91,15 +91,15 @@ states = list(IAT_clean.loc[:,'state'].unique()) #I'm getting 62 states; am I go
 df = pd.DataFrame(columns=['state', 'bias'])
 count = 0
 for st in states:
-    tmpMean = IAT_clean[IAT_clean['state']==st].mean()[3]
+    tmpMean = IAT_clean[IAT_clean['state']==st].median()[3] # Gaeun: this question seemed to require median, so 'mean -> median'
     df = df.append(pd.Series([st,tmpMean], index=df.columns), ignore_index=True)
 
 # now use the pivot_table function to calculate the same statistics
-state_bias= pd.pivot_table(IAT_clean, 'D_white_bias','state')
+state_bias= pd.pivot_table(IAT_clean, 'D_white_bias','state', aggfunc = [np.median])
 
 # make another pivot_table that calculates median bias per state, separately
 # for each race (organized by columns)
-state_race_bias = pd.pivot_table(IAT_clean, 'D_white_bias','state','race')
+state_race_bias = pd.pivot_table(IAT_clean, 'D_white_bias','state','race', aggfunc = [np.median])
 
 #%%
 # Question 4: merging and more merging --> Juliana + Gaeun
@@ -123,6 +123,7 @@ print(prop_black)
 # black/African American
 # read in this file and merge its contents with your prop_black table
 census_file = path+'/state_pop.xlsx'
+# pip install xlrd #<< Gaeun: for somebody doesn't have xlrd package yet:)
 census = pd.read_excel(census_file) #use read_excel for .xlsx files
 prop_black=prop_black.reset_index(level=['state']) #had to make state a column,
 #as it was the index in the crosstab.
@@ -130,9 +131,18 @@ prop_black=prop_black.rename(columns={'state':'State'}) #rename to match census
 merged = pd.merge(census,prop_black,on='State')
 
 # use the corr method to correlate the census proportions to the sample proportions
+correlation = np.corrcoef(merged.per_black, merged.iloc[:,5]) # Gaeun: The last column(6th) was sample proprtion. 
+# Correlation coefficient ~ .88
 
 # now merge the census data with your state_race_bias pivot table
+srB_reindex = state_race_bias.reset_index(level=['state'])
+merged2 = pd.merge(census, state_race_bias, left_on = 'State', right_on = 'state') 
+# Gaeun: when variable names for merging were different, we can try left/right_on as well.
 
 # use the corr method again to determine whether white_good biases is correlated
 # with the proportion of the population which is black across states
 # calculate and print this correlation for white and black participants
+col_forWhite = np.corrcoef(merged2.per_black, merged2.iloc[:,9])
+col_forBlack = np.corrcoef(merged2.per_black, merged2.iloc[:,8])
+print('correlation(white): ', col_forWhite[0,1], '\n' 
+      'correlation(black): ', col_forBlack[0,1])
